e6497e1cd9804b1abaf07927afd809688f3dbc70
TTtie <tttiett@outlook.com>
2019-07-14 13:55:14 +0300

    add tests

---
+++ tests/parse.js
@@ -0,0 +1,18 @@
+const { encoders: {"image/png": encode},
+    decoders: {"image/png": decode} } = require("../index.js");
+const { readFileSync } = require("fs");
+describe("parsing a static image", function() {
+    const image = decode(readFileSync("./png-image.png");
+    it("it has 0 frames", function() {
+        expect(image.frames).toBe(0);
+    });
+    it("colorType is defined", function() {
+        expect(image.colorType).toBeDefined()
+    })
+    it("colorDepth is defined", function() {
+        expect(image.colorDepth).toBeDefined()
+    });
+    it("delay is undefined", function() {
+        expect(image.delay).not.toBeDefined()
+    })
+});
